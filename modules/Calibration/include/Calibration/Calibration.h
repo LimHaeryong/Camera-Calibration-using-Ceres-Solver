3f260ed5dba75de469efb2369e33ba862bd7841c
LimHaeryong <nabi4622@snu.ac.kr>
2023-08-07 13:29:46 +0900

    feat find_homography method

--- modules/Calibration/include/Calibration/Calibration.h
+++ modules/Calibration/include/Calibration/Calibration.h
@@ -39,12 +39,10 @@ private:
   cv::Mat new_camera_matrix_;
   cv::Mat dist_coeffs_;
 
-  const uint FIND_CHESSBOARD_FLAGS = cv::CALIB_CB_ADAPTIVE_THRESH +
-                                     cv::CALIB_CB_NORMALIZE_IMAGE +
-                                     cv::CALIB_CB_FAST_CHECK;
-  const uint CALIB_FISHEYE_FLAGS = cv::fisheye::CALIB_RECOMPUTE_EXTRINSIC +
-                                   cv::fisheye::CALIB_CHECK_COND +
-                                   cv::fisheye::CALIB_FIX_SKEW;
+  const uint FIND_CHESSBOARD_FLAGS =
+    cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE + cv::CALIB_CB_FAST_CHECK;
+  const uint CALIB_FISHEYE_FLAGS =
+    cv::fisheye::CALIB_RECOMPUTE_EXTRINSIC + cv::fisheye::CALIB_CHECK_COND + cv::fisheye::CALIB_FIX_SKEW;
   const cv::TermCriteria CORNERSUBPIX_CRITERIA;
 
   cv::Mat map_x_;
@@ -62,13 +60,27 @@ private:
     const std::vector<std::vector<cv::Point2f>> &image_points,
     std::vector<cv::Mat> &homographies);
 
-  cv::Mat get_homography(
-    const std::vector<cv::Point3f> object_point,
-    const std::vector<cv::Point2f> &image_point);
+  cv::Mat get_homography(const std::vector<cv::Point3f> &object_point, const std::vector<cv::Point2f> &image_point);
 
-  void get_intrinsics(
-    const std::vector<cv::Mat> &homographies,
-    cv::Mat &camera_matrix);
+  cv::Mat find_homography(
+    const std::vector<cv::Point2f> &src_points,
+    const std::vector<cv::Point2f> &dst_points,
+    const std::vector<int> &indices);
+
+  cv::Mat find_homography_ransac(
+    const std::vector<cv::Point2f> &src_points,
+    const std::vector<cv::Point2f> &dst_points,
+    int max_iteration = 100,
+    double error_threshold = 0.3,
+    double confidence = 0.995);
+
+  void refine_homography(
+    cv::Mat &H,
+    const std::vector<cv::Point2f> &src_points,
+    const std::vector<cv::Point2f> &dst_points,
+    const std::vector<int> &indices);
+
+  void get_intrinsics(const std::vector<cv::Mat> &homographies, cv::Mat &camera_matrix);
 
   void compute_v(const cv::Mat &H, int p, int q, cv::Mat &v);
 
@@ -77,15 +89,11 @@ private:
     const std::vector<cv::Mat> &homographies,
     std::vector<cv::Mat> &extrinsics);
 
-  cv::Mat get_extrinsic(
-    const cv::Mat &camera_matrix,
-    const cv::Mat &homography);
+  cv::Mat get_extrinsic(const cv::Mat &camera_matrix, const cv::Mat &homography);
 
   void refine_rotation_matrix(cv::Mat &R);
 
-  cv::Vec3d get_normalized_coord(
-    const cv::Point3f &object_point,
-    const cv::Mat &extrinsic);
+  cv::Vec3d get_normalized_coord(const cv::Point3f &object_point, const cv::Mat &extrinsic);
 
   double refine_all(
     cv::Mat &camera_matrix,
@@ -94,20 +102,43 @@ private:
     const std::vector<std::vector<cv::Point3f>> &object_points,
     const std::vector<std::vector<cv::Point2f>> &image_points);
 
+  struct projection_error
+  {
+    projection_error(const cv::Point2f &src_point, const cv::Point2f &dst_point)
+    : src_point(src_point), dst_point(dst_point)
+    {
+    }
+
+    template<typename T>
+    bool operator()(const T *const H, T *residuals) const
+    {
+      T pred_z = H[6] * T(src_point.x) + H[7] * T(src_point.y) + H[8];
+      T predict[2];
+      predict[0] = (H[0] * T(src_point.x) + H[1] * T(src_point.y) + H[2]) / pred_z;
+      predict[1] = (H[3] * T(src_point.x) + H[4] * T(src_point.y) + H[5]) / pred_z;
+      residuals[0] = predict[0] - T(dst_point.x);
+      residuals[1] = predict[1] - T(dst_point.y);
+      return true;
+    }
+
+    static ceres::CostFunction *create(const cv::Point2f &src_point, const cv::Point2f &dst_point)
+    {
+      return (new ceres::AutoDiffCostFunction<projection_error, 2, 9>(new projection_error(src_point, dst_point)));
+    }
+
+    const cv::Point2f src_point;
+    const cv::Point2f dst_point;
+  };
+
   struct reprojection_error
   {
-    reprojection_error(
-      const cv::Point2f &image_point,
-      const cv::Point3f &object_point)
+    reprojection_error(const cv::Point2f &image_point, const cv::Point3f &object_point)
     : image_point(image_point), object_point(object_point)
     {
     }
 
     template<typename T>
-    bool operator()(
-      const T *const intrinsic,
-      const T *const extrinsic,
-      T *residuals) const
+    bool operator()(const T *const intrinsic, const T *const extrinsic, T *residuals) const
     {
       T X[3];
       T object_point_[3];
@@ -125,10 +156,8 @@ private:
       T r = ceres::sqrt(x_p * x_p + y_p * y_p);
       T theta = ceres::atan(r);
       T fisheye_distortion = theta / r *
-                             (1.0 + intrinsic[5] * ceres::pow(theta, 2) +
-                              intrinsic[6] * ceres::pow(theta, 4) +
-                              intrinsic[7] * ceres::pow(theta, 6) +
-                              intrinsic[8] * ceres::pow(theta, 8));
+                             (1.0 + intrinsic[5] * ceres::pow(theta, 2) + intrinsic[6] * ceres::pow(theta, 4) +
+                              intrinsic[7] * ceres::pow(theta, 6) + intrinsic[8] * ceres::pow(theta, 8));
 
       // with skew
       // T predict_x = intrinsic[0] * (fisheye_distortion * x_p + intrinsic[2] *
@@ -144,9 +173,7 @@ private:
       return true;
     }
 
-    static ceres::CostFunction *create(
-      const cv::Point2f &image_point,
-      const cv::Point3f &object_point)
+    static ceres::CostFunction *create(const cv::Point2f &image_point, const cv::Point3f &object_point)
     {
       return (new ceres::AutoDiffCostFunction<reprojection_error, 2, 9, 6>(
         new reprojection_error(image_point, object_point)));
--- modules/Calibration/src/Calibration.cpp
+++ modules/Calibration/src/Calibration.cpp
@@ -1,16 +1,16 @@
 #include "Calibration/Calibration.h"
 
+#include <numeric>
+#include <random>
+#include <set>
+
 Calibration::Calibration(
   const std::string &image_directory,
   const std::string &distortion_model,
   int corners_width,
   int corners_height,
   double square)
-: square_(square),
-  CORNERSUBPIX_CRITERIA(cv::TermCriteria(
-    cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER,
-    30,
-    0.1))
+: square_(square), CORNERSUBPIX_CRITERIA(cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER, 30, 0.1))
 {
   cv::glob(image_directory + "/*.jpg", image_paths_);
   cv::glob(image_directory + "/*.png", image_paths_);
@@ -43,10 +43,8 @@ Calibration::Calibration(const YAML::Node &config)
     fisheye_ = false;
   }
 
-  std::vector<double> camera_matrix =
-    config["camera_matrix"].as<std::vector<double>>();
-  std::vector<double> dist_coeffs =
-    config["dist_coeffs"].as<std::vector<double>>();
+  std::vector<double> camera_matrix = config["camera_matrix"].as<std::vector<double>>();
+  std::vector<double> dist_coeffs = config["dist_coeffs"].as<std::vector<double>>();
 
   image_size_.width = config["image_width"].as<int>();
   image_size_.height = config["image_height"].as<int>();
@@ -57,35 +55,15 @@ Calibration::Calibration(const YAML::Node &config)
   if (fisheye_)
   {
     cv::fisheye::estimateNewCameraMatrixForUndistortRectify(
-      camera_matrix_,
-      dist_coeffs_,
-      image_size_,
-      cv::Mat(),
-      new_camera_matrix_,
-      0.0);
+      camera_matrix_, dist_coeffs_, image_size_, cv::Mat(), new_camera_matrix_, 0.0);
     cv::fisheye::initUndistortRectifyMap(
-      camera_matrix_,
-      dist_coeffs_,
-      cv::Mat(),
-      new_camera_matrix_,
-      image_size_,
-      CV_32FC1,
-      map_x_,
-      map_y_);
+      camera_matrix_, dist_coeffs_, cv::Mat(), new_camera_matrix_, image_size_, CV_32FC1, map_x_, map_y_);
   }
   else
   {
-    new_camera_matrix_ = cv::getOptimalNewCameraMatrix(
-      camera_matrix_, dist_coeffs_, image_size_, 0.0);
+    new_camera_matrix_ = cv::getOptimalNewCameraMatrix(camera_matrix_, dist_coeffs_, image_size_, 0.0);
     cv::initUndistortRectifyMap(
-      camera_matrix_,
-      dist_coeffs_,
-      cv::Mat(),
-      new_camera_matrix_,
-      image_size_,
-      CV_32FC1,
-      map_x_,
-      map_y_);
+      camera_matrix_, dist_coeffs_, cv::Mat(), new_camera_matrix_, image_size_, CV_32FC1, map_x_, map_y_);
   }
 }
 
@@ -103,14 +81,12 @@ void Calibration::calibrate()
   {
     corners.clear();
     cv::Mat image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
-    bool ret = cv::findChessboardCorners(
-      image, pattern_size_, corners, FIND_CHESSBOARD_FLAGS);
+    bool ret = cv::findChessboardCorners(image, pattern_size_, corners, FIND_CHESSBOARD_FLAGS);
     if (ret == false)
     {
       continue;
     }
-    cv::cornerSubPix(
-      image, corners, cv::Size(7, 7), cv::Size(-1, -1), CORNERSUBPIX_CRITERIA);
+    cv::cornerSubPix(image, corners, cv::Size(7, 7), cv::Size(-1, -1), CORNERSUBPIX_CRITERIA);
     image_points.push_back(std::move(corners));
   }
 
@@ -122,34 +98,25 @@ void Calibration::calibrate()
     for (int j = 0; j < pattern_size_.width; ++j)
     {
       width = static_cast<double>(j) * square_;
-      object_point[i * pattern_size_.width + j] =
-        cv::Point3f(height, width, 0.0);
+      object_point[i * pattern_size_.width + j] = cv::Point3f(height, width, 0.0);
     }
   }
 
-  std::vector<std::vector<cv::Point3f>> object_points(
-    image_points.size(), object_point);
+  std::vector<std::vector<cv::Point3f>> object_points(image_points.size(), object_point);
 
   std::vector<cv::Mat> rvecs, tvecs;
   double reprojection_error;
   if (fisheye_)
   {
-    reprojection_error = calibrate_implement(
-      object_points, image_points, image_size_, camera_matrix_, dist_coeffs_);
+    reprojection_error = calibrate_implement(object_points, image_points, image_size_, camera_matrix_, dist_coeffs_);
     // reprojection_error = cv::fisheye::calibrate(
     //     object_points, image_points, image_size_, camera_matrix_,
     //     dist_coeffs_, rvecs, tvecs, CALIB_FISHEYE_FLAGS);
   }
   else
   {
-    reprojection_error = cv::calibrateCamera(
-      object_points,
-      image_points,
-      image_size_,
-      camera_matrix_,
-      dist_coeffs_,
-      rvecs,
-      tvecs);
+    reprojection_error =
+      cv::calibrateCamera(object_points, image_points, image_size_, camera_matrix_, dist_coeffs_, rvecs, tvecs);
   }
 
   std::cout << "reprojection_error : " << reprojection_error << std::endl;
@@ -159,35 +126,15 @@ void Calibration::calibrate()
   if (fisheye_)
   {
     cv::fisheye::estimateNewCameraMatrixForUndistortRectify(
-      camera_matrix_,
-      dist_coeffs_,
-      image_size_,
-      cv::Mat(),
-      new_camera_matrix_,
-      0.0);
+      camera_matrix_, dist_coeffs_, image_size_, cv::Mat(), new_camera_matrix_, 0.0);
     cv::fisheye::initUndistortRectifyMap(
-      camera_matrix_,
-      dist_coeffs_,
-      cv::Mat(),
-      new_camera_matrix_,
-      image_size_,
-      CV_32FC1,
-      map_x_,
-      map_y_);
+      camera_matrix_, dist_coeffs_, cv::Mat(), new_camera_matrix_, image_size_, CV_32FC1, map_x_, map_y_);
   }
   else
   {
-    new_camera_matrix_ = cv::getOptimalNewCameraMatrix(
-      camera_matrix_, dist_coeffs_, image_size_, 0.0);
+    new_camera_matrix_ = cv::getOptimalNewCameraMatrix(camera_matrix_, dist_coeffs_, image_size_, 0.0);
     cv::initUndistortRectifyMap(
-      camera_matrix_,
-      dist_coeffs_,
-      cv::Mat(),
-      new_camera_matrix_,
-      image_size_,
-      CV_32FC1,
-      map_x_,
-      map_y_);
+      camera_matrix_, dist_coeffs_, cv::Mat(), new_camera_matrix_, image_size_, CV_32FC1, map_x_, map_y_);
   }
 }
 
@@ -212,12 +159,10 @@ void Calibration::save_yaml()
   config["image_width"] = image_size_.width;
   config["image_height"] = image_size_.height;
 
-  std::vector<double> camera_matrix(
-    camera_matrix_.begin<double>(), camera_matrix_.end<double>());
+  std::vector<double> camera_matrix(camera_matrix_.begin<double>(), camera_matrix_.end<double>());
   config["camera_matrix"] = camera_matrix;
 
-  std::vector<double> dist_coeffs(
-    dist_coeffs_.begin<double>(), dist_coeffs_.end<double>());
+  std::vector<double> dist_coeffs(dist_coeffs_.begin<double>(), dist_coeffs_.end<double>());
   config["dist_coeffs"] = dist_coeffs;
 
   YAML::Emitter emitter;
@@ -238,9 +183,7 @@ double Calibration::calibrate_implement(
   get_intrinsics(homographies, camera_matrix);
   get_extrinsics(camera_matrix, homographies, extrinsics);
   dist_coeffs = cv::Mat::zeros(4, 1, CV_64F);
-  double reprojection_error = refine_all(
-    camera_matrix, dist_coeffs, extrinsics, object_points, image_points);
-
+  double reprojection_error = refine_all(camera_matrix, dist_coeffs, extrinsics, object_points, image_points);
   return reprojection_error;
 }
 
@@ -261,7 +204,7 @@ void Calibration::get_homographies(
 }
 
 cv::Mat Calibration::get_homography(
-  const std::vector<cv::Point3f> object_point,
+  const std::vector<cv::Point3f> &object_point,
   const std::vector<cv::Point2f> &image_point)
 {
   std::size_t N = object_point.size();
@@ -271,13 +214,130 @@ cv::Mat Calibration::get_homography(
     object_point_planar[j].x = object_point[j].x;
     object_point_planar[j].y = object_point[j].y;
   }
-  cv::Mat homography = cv::findHomography(object_point_planar, image_point);
+
+  cv::Mat homography = cv::findHomography(object_point_planar, image_point, cv::RANSAC);
+  //cv::Mat homography2 = find_homography_ransac(object_point_planar, image_point, 2000, 3.0);
+
   return homography;
 }
 
-void Calibration::get_intrinsics(
-  const std::vector<cv::Mat> &homographies,
-  cv::Mat &camera_matrix)
+cv::Mat Calibration::find_homography(
+  const std::vector<cv::Point2f> &src_points,
+  const std::vector<cv::Point2f> &dst_points,
+  const std::vector<int> &indices)
+{
+  std::size_t num_points = indices.size();
+  cv::Mat A = cv::Mat::zeros(2 * num_points, 9, CV_64F);
+
+  for (std::size_t i = 0; i < num_points; ++i)
+  {
+    auto &src = src_points[indices[i]];
+    auto &dst = dst_points[indices[i]];
+    A.at<double>(2 * i, 0) = src.x;
+    A.at<double>(2 * i, 1) = src.y;
+    A.at<double>(2 * i, 2) = 1.0;
+    A.at<double>(2 * i, 6) = -1.0 * dst.x * src.x;
+    A.at<double>(2 * i, 7) = -1.0 * dst.x * src.y;
+    A.at<double>(2 * i, 8) = -1.0 * dst.x;
+    A.at<double>(2 * i + 1, 3) = src.x;
+    A.at<double>(2 * i + 1, 4) = src.y;
+    A.at<double>(2 * i + 1, 5) = 1.0;
+    A.at<double>(2 * i + 1, 6) = -1.0 * dst.y * src.x;
+    A.at<double>(2 * i + 1, 7) = -1.0 * dst.y * src.y;
+    A.at<double>(2 * i + 1, 8) = -1.0 * dst.y;
+  }
+  cv::Mat S, U, Vt;
+  cv::SVD::compute(A.t() * A, S, U, Vt, cv::SVD::Flags::FULL_UV);
+  cv::Mat H = cv::Mat(3, 3, CV_64F, Vt.row(Vt.rows - 1).data);
+  return H;
+}
+
+void Calibration::refine_homography(
+  cv::Mat &H,
+  const std::vector<cv::Point2f> &src_points,
+  const std::vector<cv::Point2f> &dst_points,
+  const std::vector<int> &indices)
+{
+  double *H_ptr = H.ptr<double>();
+
+  ceres::Problem problem;
+  std::size_t num_points = indices.size();
+  for (std::size_t i = 0; i < num_points; ++i)
+  {
+    ceres::CostFunction *cost_function = projection_error::create(src_points[indices[i]], dst_points[indices[i]]);
+    problem.AddResidualBlock(cost_function, NULL, H_ptr);
+  }
+
+  ceres::Solver::Options options;
+  options.linear_solver_type = ceres::ITERATIVE_SCHUR;
+  options.num_threads = 8;
+  options.minimizer_progress_to_stdout = false;
+  ceres::Solver::Summary summary;
+  ceres::Solve(options, &problem, &summary);
+}
+
+cv::Mat Calibration::find_homography_ransac(
+  const std::vector<cv::Point2f> &src_points,
+  const std::vector<cv::Point2f> &dst_points,
+  int max_iteration,
+  double error_threshold,
+  double confidence)
+{
+  std::size_t num_points = src_points.size();
+
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  std::uniform_int_distribution<int> distribution(0, num_points - 1);
+  std::set<int> random_numbers;
+  std::vector<int> inliers, inliers_tmp;
+  inliers.reserve(num_points);
+  inliers_tmp.reserve(num_points);
+
+  int iter = 0;
+  while (iter++ < max_iteration)
+  {
+    random_numbers.clear();
+    inliers_tmp.clear();
+    while (random_numbers.size() < 4)
+    {
+      int index = distribution(gen);
+      random_numbers.insert(index);
+    }
+
+    cv::Mat H_tmp =
+      find_homography(src_points, dst_points, std::vector<int>(random_numbers.begin(), random_numbers.end()));
+
+    for (std::size_t i = 0; i < num_points; ++i)
+    {
+      const auto &src = src_points[i];
+      const auto &dst = dst_points[i];
+      double z = src.x * H_tmp.at<double>(2, 0) + src.y * H_tmp.at<double>(2, 1) + H_tmp.at<double>(2, 2);
+
+      double x_diff =
+        (H_tmp.at<double>(0, 0) * src.x + H_tmp.at<double>(0, 1) * src.y + H_tmp.at<double>(0, 2)) / z - dst.x;
+      double y_diff =
+        (H_tmp.at<double>(1, 0) * src.x + H_tmp.at<double>(1, 1) * src.y + H_tmp.at<double>(1, 2)) / z - dst.y;
+      double sqaured_error = x_diff * x_diff + y_diff * y_diff;
+      if (sqaured_error < error_threshold)
+      {
+        inliers_tmp.push_back(i);
+      }
+    }
+    if (inliers.size() < inliers_tmp.size())
+    {
+      inliers.swap(inliers_tmp);
+      if (inliers.size() > num_points * confidence)
+        break;
+    }
+
+  }
+  cv::Mat H = find_homography(src_points, dst_points, inliers);
+  refine_homography(H, src_points, dst_points, inliers);
+  H /= H.at<double>(2, 2);
+  return H;
+}
+
+void Calibration::get_intrinsics(const std::vector<cv::Mat> &homographies, cv::Mat &camera_matrix)
 {
   std::size_t M = homographies.size();
   cv::Mat V = cv::Mat::zeros(2 * M, 6, CV_64F);
@@ -301,8 +361,7 @@ void Calibration::get_intrinsics(
   cv::Vec6d b = svd.vt.row(svd.vt.rows - 1);
 
   camera_matrix = cv::Mat::zeros(3, 3, CV_64F);
-  double w = b[0] * b[2] * b[5] - b[1] * b[1] * b[5] - b[0] * b[4] * b[4] +
-             2 * b[1] * b[3] * b[4] - b[2] * b[3] * b[3];
+  double w = b[0] * b[2] * b[5] - b[1] * b[1] * b[5] - b[0] * b[4] * b[4] + 2 * b[1] * b[3] * b[4] - b[2] * b[3] * b[3];
   double d = b[0] * b[2] - b[1] * b[1];
   camera_matrix.at<double>(0, 0) = sqrt(w / (d * b[0]));
   camera_matrix.at<double>(1, 1) = sqrt(w / (d * d) * b[0]);
@@ -316,13 +375,10 @@ void Calibration::get_intrinsics(
 void Calibration::compute_v(const cv::Mat &H, int p, int q, cv::Mat &v)
 {
   v.at<double>(0, 0) = H.at<double>(0, p) * H.at<double>(0, q);
-  v.at<double>(0, 1) = H.at<double>(0, p) * H.at<double>(1, q) +
-                       H.at<double>(1, p) * H.at<double>(0, q);
+  v.at<double>(0, 1) = H.at<double>(0, p) * H.at<double>(1, q) + H.at<double>(1, p) * H.at<double>(0, q);
   v.at<double>(0, 2) = H.at<double>(1, p) * H.at<double>(1, q);
-  v.at<double>(0, 3) = H.at<double>(2, p) * H.at<double>(0, q) +
-                       H.at<double>(0, p) * H.at<double>(2, q);
-  v.at<double>(0, 4) = H.at<double>(2, p) * H.at<double>(1, q) +
-                       H.at<double>(1, p) * H.at<double>(2, q);
+  v.at<double>(0, 3) = H.at<double>(2, p) * H.at<double>(0, q) + H.at<double>(0, p) * H.at<double>(2, q);
+  v.at<double>(0, 4) = H.at<double>(2, p) * H.at<double>(1, q) + H.at<double>(1, p) * H.at<double>(2, q);
   v.at<double>(0, 5) = H.at<double>(2, p) * H.at<double>(2, q);
 }
 
@@ -341,9 +397,7 @@ void Calibration::get_extrinsics(
   }
 }
 
-cv::Mat Calibration::get_extrinsic(
-  const cv::Mat &camera_matrix,
-  const cv::Mat &homography)
+cv::Mat Calibration::get_extrinsic(const cv::Mat &camera_matrix, const cv::Mat &homography)
 {
   cv::Mat A_inv = camera_matrix.inv();
   cv::Mat R(3, 3, CV_64F);
@@ -370,9 +424,7 @@ void Calibration::refine_rotation_matrix(cv::Mat &R)
   R = U * Vt;
 }
 
-cv::Vec3d Calibration::get_normalized_coord(
-  const cv::Point3f &object_point,
-  const cv::Mat &extrinsic)
+cv::Vec3d Calibration::get_normalized_coord(const cv::Point3f &object_point, const cv::Mat &extrinsic)
 {
   cv::Vec4d X;
   X[0] = object_point.x;
@@ -426,10 +478,8 @@ double Calibration::refine_all(
   {
     for (std::size_t j = 0; j < N; ++j)
     {
-      ceres::CostFunction *cost_function =
-        reprojection_error::create(image_points[i][j], object_points[i][j]);
-      problem.AddResidualBlock(
-        cost_function, NULL, intrinsic_vec.data(), extrinsics_vec[i].data());
+      ceres::CostFunction *cost_function = reprojection_error::create(image_points[i][j], object_points[i][j]);
+      problem.AddResidualBlock(cost_function, NULL, intrinsic_vec.data(), extrinsics_vec[i].data());
     }
   }
 
@@ -449,7 +499,7 @@ double Calibration::refine_all(
   dist_coeffs.at<double>(1, 0) = intrinsic_vec[6];
   dist_coeffs.at<double>(2, 0) = intrinsic_vec[7];
   dist_coeffs.at<double>(3, 0) = intrinsic_vec[8];
-  
+
   double squared_sum = 0.0;
   for (std::size_t i = 0; i < M; ++i)
   {
@@ -463,7 +513,8 @@ double Calibration::refine_all(
 
     std::vector<cv::Point2f> projected_points;
     cv::fisheye::projectPoints(object_points[i], projected_points, rvec, tvec, camera_matrix, dist_coeffs);
-    for(std::size_t j = 0; j < N; ++j){
+    for (std::size_t j = 0; j < N; ++j)
+    {
       cv::Point2f point_diff = projected_points[j] - image_points[i][j];
       squared_sum += cv::pow(point_diff.x, 2) + cv::pow(point_diff.y, 2);
     }
